82f3552d5f5cd178ec1c47de14d444a9d6e75991
Chaerin <qawse@gmail.com>
2020-03-08 23:03:16 +0900

    Modify settings.py

--- zol_1/config/settings.py
+++ zol_1/config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER':  'CHAERIN',
+        'PASSWORD': 'qawse9845',
+        'HOST': '13.209.73.42',
+        'PORT': '3306',
     }
 }
 
